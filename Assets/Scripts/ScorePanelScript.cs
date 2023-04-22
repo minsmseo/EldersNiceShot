8a482031fec273c4b93dd5d1400a5ac3ab4d6c87
goodbyebada <goodbyebada@naver.com>
2023-04-22 21:42:28 +0900

    make gameScore UI

---
+++ Assets/Scripts/ScorePanelScript.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using TMPro;
+
+public class ScorePanelScript : MonoBehaviour
+{
+    public GameObject pauseButton;
+    public TextMeshProUGUI scoreText;
+
+    /**
+     화면 상단 우측에 보이는 스코어 텍스트 밑 일시정지 버튼을 클릭했을 때 수행할 동작
+     */
+    public void pause_btn_clicked()
+    {
+
+    }
+
+    /**
+     화면 우측 상단에 있는 스코어 텍스트를 업데이트하는 함수
+     */
+    private void change_scoreText(string score)
+    {
+        scoreText.text = "score: " + score;
+
+    }
+
+    public void change_scoreText(int score)
+    {
+        change_scoreText(score.ToString());
+    }
+}
