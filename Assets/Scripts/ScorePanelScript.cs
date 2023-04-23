80bbb3b38f178c2d5f96d0478a31b4409c47045e
goodbyebada <goodbyebada@naver.com>
2023-04-24 00:01:22 +0900

    make MainPanelUI

--- Assets/Scripts/ScorePanelScript.cs
+++ Assets/Scripts/ScorePanelScript.cs
@@ -3,15 +3,21 @@ using TMPro;
 
 public class ScorePanelScript : MonoBehaviour
 {
+
+
     public GameObject pauseButton;
     public TextMeshProUGUI scoreText;
+    public GameObject mainPanel;
+    public GameObject scorePanel;
+
 
     /**
      화면 상단 우측에 보이는 스코어 텍스트 밑 일시정지 버튼을 클릭했을 때 수행할 동작
      */
     public void pause_btn_clicked()
     {
-
+        mainPanel.SetActive(true);
+        scorePanel.SetActive(false);
     }
 
     /**
