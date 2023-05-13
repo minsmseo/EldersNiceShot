5d03024811bb2d2e32d54f2ac363ce03f2610bd4
goodbyebada <goodbyebada@naver.com>
2023-05-14 08:53:37 +0900

    로비씬 레벨, 스테이지 선택

---
+++ Assets/Scripts/UIScripts/ChooseLVPanel.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ChooseLVPanel : MonoBehaviour
+{
+
+    public GameObject buttonExit;
+    public GameObject level1;
+    public GameObject level2;
+    public GameObject chooseLVPanel;
+    public GameObject selectPanel;
+
+    public GameObject stageImage;
+    private int currentLevel;
+
+    //private Button level1Button;
+    //private Button level2Button;
+    public Button level1Button;
+    public Button level2Button;
+    public Button exitButton;
+
+
+    //private Image stageImageImageComponent;
+    //private Sprite[] previewSprites;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+        //if (stageImage != null)
+        //{
+        //    //이미지가 없다면 이미지 컴포턴트에서 가져온다 
+        //    stageImageImageComponent = stageImage.GetComponent<Image>();
+        //}
+        //stageCount = 2;
+        //previewSprites = new Sprite[stageCount];
+        //currentStage = 1;
+
+        chooseLVPanel.SetActive(true);
+        selectPanel.SetActive(false);
+
+        //level1Button = level1.GetComponent<Button>();
+        //level2Button = level2.GetComponent<Button>();
+
+        level1Button.onClick.AddListener(choose_level1);
+        level2Button.onClick.AddListener(choose_level2);
+        exitButton.onClick.AddListener(buttonExit_clicked);
+
+
+
+
+    }
+
+
+
+    //public void stageimage_clicked()
+    //{
+
+    //}
+
+
+
+    public void choose_level1()
+    {
+        selectPanel.SetActive(true);
+        chooseLVPanel.SetActive(false);
+    }
+
+    public void choose_level2()
+    {
+        selectPanel.SetActive(true);
+        chooseLVPanel.SetActive(false);
+    }
+
+
+
+    //private void set_stage_image(int stage)
+    //{
+    //    int index = stage - 1;
+    //    if (index < 0 || stageCount <= index || stageImageImageComponent == null) return;
+    //    stageImageImageComponent.sprite = previewSprites[index];
+    //}
+
+    public void buttonExit_clicked()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit(); // ���ø����̼� ����
+#endif
+    }
+
+
+}
+
---
+++ Assets/Scripts/UIScripts/SelectPanel.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class SelectPanel : MonoBehaviour
+{
+
+
+    public GameObject buttonLeft;
+    public GameObject buttonRight;
+    public GameObject buttonBack;
+    public TextMeshProUGUI stageText;
+
+    public GameObject selectPanel;
+    public GameObject chooseLVPanel;
+
+    public GameObject stageImage;
+    private Image stageImageImageComponent;
+   
+    //public GameObject startPanel;
+
+    private int currentStage;
+    private int stageCount;
+    private Sprite[] previewSprites;
+
+   
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+        //if (stageImage != null)
+        //{
+        //    //이미지가 없다면 이미지 컴포턴트에서 가져온다 
+        //    stageImageImageComponent = stageImage.GetComponent<Image>();
+        //}
+        stageCount = 2;
+        //previewSprites = new Sprite[stageCount];
+        currentStage = 1;
+
+        buttonLeft.GetComponent<Button>().onClick.AddListener(buttonLeft_clicked);
+        buttonRight.GetComponent<Button>().onClick.AddListener(buttonRight_clicked);
+        buttonBack.GetComponent<Button>().onClick.AddListener(buttonBack_clicked);
+        stageImage.GetComponent<Button>().onClick.AddListener(move_to_Game);
+
+        
+       
+
+
+
+
+    }
+
+    public void move_to_Game()
+    {
+        SceneManager.LoadScene("Stage_1");
+    }
+
+
+    public void buttonBack_clicked()
+    {
+        chooseLVPanel.SetActive(true);
+        selectPanel.SetActive(false);
+    }
+
+    public void buttonLeft_clicked()
+    {
+        currentStage += stageCount;
+        currentStage--;
+        currentStage %= stageCount;
+        change_stage(currentStage);
+
+
+    }
+
+    public void buttonRight_clicked()
+    {
+        currentStage++;
+        currentStage %= stageCount;
+        change_stage(currentStage);
+
+    }
+
+    public void stageImage_clicked()
+    {
+
+    }
+
+    public void change_stage(int stage)
+    {
+
+        //set_stage_image(stage);
+        set_stage_text(stage);
+    }
+
+    private void set_stage_text(int stage)
+    {
+        int stageNum = stage + 1;
+        stageText.text = "stage: " + stageNum.ToString();
+    }
+
+    //private void set_stage_image(int stage)
+    //{
+    //    int index = stage - 1;
+    //    if (index < 0 || stageCount <= index || stageImageImageComponent == null) return;
+    //    stageImageImageComponent.sprite = previewSprites[index];
+    //}
+
+    public void buttonExit_clicked()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit(); // ���ø����̼� ����
+#endif
+    }
+
+
+}
